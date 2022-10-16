63f1ac1c2b2f6ad82df571f2ff8945c99a8d0ed8
lisic4kin <lisitsin-shura@mail.ru>
2022-10-16 21:33:26 +0300

    Создание сервиса для профиля пользователя

--- src/main/java/b2b/rsatu/portalservice/repository/base/BaseReferenceRepository.java
+++ src/main/java/b2b/rsatu/portalservice/repository/base/BaseReferenceRepository.java
@@ -2,9 +2,9 @@ package b2b.rsatu.portalservice.repository.base;
 
 import b2b.rsatu.portalservice.entity.base.BaseReferenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-@Repository
+@NoRepositoryBean
 public interface BaseReferenceRepository<E extends BaseReferenceEntity> extends JpaRepository<E, Long> {
 
 }
